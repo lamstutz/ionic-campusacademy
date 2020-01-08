137c92fd4b600be22673bdb6b8e6e2a01fb0eb1e
louis amstutz <louis.amstutz@gmail.com>
2020-01-08 09:10:11 +0100

    test 1

---
+++ src/app/my-component/my-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-component',
+  templateUrl: 'my-component.component.html',
+  styleUrls: ['my-component.component.scss']
+})
+export class MyComponentComponent {
+
+  constructor() { }
+
+}
--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
+import { MyComponentComponent } from '../my-component/my-component.component';
 
 @NgModule({
   imports: [
@@ -12,6 +13,6 @@ import { Tab1Page } from './tab1.page';
     FormsModule,
     RouterModule.forChild([{ path: '', component: Tab1Page }])
   ],
-  declarations: [Tab1Page]
+  declarations: [Tab1Page, MyComponentComponent]
 })
-export class Tab1PageModule {}
+export class Tab1PageModule { }
